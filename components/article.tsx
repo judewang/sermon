fd7f39af32c8bd2c012e5c28e32133bd513763d9
Jude Wang <jude@coramdeo.cc>
2024-05-19 20:48:46 +0900

    perf: fix english word beak issue

--- components/article.tsx
+++ components/article.tsx
@@ -1,8 +1,15 @@
+import { cn } from "@/lib/utils";
 import { PropsWithChildren } from "react";
 
 export function Article({ children }: PropsWithChildren) {
   return (
-    <article className="container prose prose-xl prose-zinc bg-white py-6 md:prose-2xl prose-headings:mt-0 prose-h1:mb-3">
+    <article
+      className={cn(
+        "container prose prose-xl prose-zinc bg-white py-6",
+        "md:prose-2xl prose-headings:mt-0 prose-h1:mb-3",
+        "break-words [word-break:break-word]",
+      )}
+    >
       {children}
     </article>
   );
