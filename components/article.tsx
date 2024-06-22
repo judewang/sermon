03503437c6994a60164d8f773e080652a5aa2325
Jude Wang <jude@coramdeo.cc>
2024-06-22 18:14:09 +0900

    fix: fix wrapper horizontal padding

--- components/article.tsx
+++ components/article.tsx
@@ -5,7 +5,7 @@ export function Article({ children }: PropsWithChildren) {
   return (
     <article
       className={cn(
-        "container prose prose-xl prose-zinc bg-white py-6",
+        "container prose prose-xl prose-zinc bg-white px-0 py-6",
         "md:prose-2xl prose-headings:mt-0 prose-h1:mb-3",
         "break-words [word-break:break-word]",
       )}
