305d7f91ecbfb3cc59d813ab0a0daf41897c65dd
Jude Wang <jude@coramdeo.cc>
2024-09-02 15:11:35 +0900

    chore: remove PAPAGO from env

--- lib/env.ts
+++ lib/env.ts
@@ -2,28 +2,25 @@ import { createEnv } from "@t3-oss/env-nextjs";
 import { z } from "zod";
 
 export const env = createEnv({
-  server: {
-    PAPAGO_API_URL: z.string().url(),
-    PAPAGO_CLIENT_ID: z.string(),
-    PAPAGO_SECRET: z.string(),
-    PERPLEXITY_API_KEY: z.string(),
-    BASE_URL: z
-      .string()
-      .url()
-      .default(
-        process.env.VERCEL_URL
-          ? `https://${process.env.VERCEL_URL}`
-          : "http://localhost:3000",
-      ),
-    NODE_ENV: z.string().default("development"),
-  },
-  client: {
-    NEXT_PUBLIC_DEV_MODE: z
-      .string()
-      .optional()
-      .transform((s) => s === "true" || s === "1"),
-  },
-  experimental__runtimeEnv: {
-    NEXT_PUBLIC_DEV_MODE: process.env.NEXT_PUBLIC_DEV_MODE,
-  },
+	server: {
+		PERPLEXITY_API_KEY: z.string(),
+		BASE_URL: z
+			.string()
+			.url()
+			.default(
+				process.env.VERCEL_URL
+					? `https://${process.env.VERCEL_URL}`
+					: "http://localhost:3000",
+			),
+		NODE_ENV: z.string().default("development"),
+	},
+	client: {
+		NEXT_PUBLIC_DEV_MODE: z
+			.string()
+			.optional()
+			.transform((s) => s === "true" || s === "1"),
+	},
+	experimental__runtimeEnv: {
+		NEXT_PUBLIC_DEV_MODE: process.env.NEXT_PUBLIC_DEV_MODE,
+	},
 });
